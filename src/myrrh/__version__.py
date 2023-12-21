8958216c26c14629dc054048f4ff1eb6291837d6
PyAnjel7 <pyanjel7@gmail.com>
2023-12-21 17:46:10 +0100

    set version

--- src/myrrh/__version__.py
+++ src/myrrh/__version__.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev53+g4cb50b3.d20231221'
-__version_tuple__ = version_tuple = (0, 1, 'dev53', 'g4cb50b3.d20231221')
+__version__ = version = '0.1.0'
+__version_tuple__ = version_tuple = (0, 1, 0)
